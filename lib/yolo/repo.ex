60db974b1da84ab3a4431cffbfd97780b577fa9a
Henry <henrysavit@gmail.com>
2019-12-07 15:21:43 -0500

    Initial commit

---
+++ lib/yolo/repo.ex
@@ -0,0 +1,10 @@
+defmodule Yolo.Repo do
+  use Ecto.Repo,
+    otp_app: :yolo,
+    adapter: Ecto.Adapters.Postgres,
+    pool_size: 10
+
+  def init(_type, config) do
+    {:ok, Keyword.put(config, :url, System.get_env("DATABASE_URL"))}
+  end
+end
