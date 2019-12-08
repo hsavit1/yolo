faddf73d323a296dc26f84b217fc415e4447ad01
Henry <henrysavit@gmail.com>
2019-12-07 23:33:33 -0500

    Add about route

--- lib/yolo_web/controllers/page_controller.ex
+++ lib/yolo_web/controllers/page_controller.ex
@@ -4,4 +4,13 @@ defmodule YoloWeb.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  def about(conn, _params) do
+    render(conn, "about.html")
+  end
+
+  def resume(conn, _params) do
+    render(conn, "resume.html")
+  end
+
 end
